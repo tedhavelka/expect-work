fc7b76c8439a7ad11f8c4c446bce6a5aed2ff7a9
Ted Havelka <ted@cs.pdx.edu>
2018-02-01 15:17:07 +1100

    2018-01-31 - Wednesday, Ted committing modest exploratory Tcl code in capture script file, code which demonstrates Tcl procedures and first successful-in-this-project pattern matches.  - TMH

--- capture-remote-result.sh
+++ capture-remote-result.sh
@@ -17,6 +17,39 @@
 
 
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+## - SECTION - Tcl procedures
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+proc show_diag_tcl {caller message options} {
+    puts "$caller:  $message"
+}
+
+
+proc token_count {caller tokens} {
+    puts "caller '$caller' sends string '$tokens' of [llength $tokens]"
+}
+
+
+proc set_expect_out_pattern_matches {} {
+    puts "tcl procedure:  setting expect_out pattern match strings to some values,"
+
+    set expect_out(1,string) 101
+    set expect_out(2,string) 102
+    set expect_out(3,string) 103
+    set expect_out(4,string) 104
+    set expect_out(5,string) 105
+
+    set expect_out(6,string) 106
+    set expect_out(7,string) 107
+    set expect_out(8,string) 108
+    set expect_out(9,string) 109
+
+    puts "tcl procedure:  returning to caller . . ."
+}
+
+
+
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 ## - SECTION - store parameters from command line
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
@@ -30,6 +63,8 @@ set es_passphrase [lindex $argv 2]
 set es_remote_command [lindex $argv 3]
 
 set es_logfile "./z"
+
+set ls_command "ls -f -d \[abcDM\]*\r"
 
 
 
@@ -49,11 +84,34 @@ puts "expect script starting,"
 # puts " "
 puts "1) expect script timeout set to $timeout seconds,"
 puts "2) variable \$user is string of [string bytelength $es_user],"
+## puts "3)"; puts [token_count main "ye to des hai tera"]"
+# set a [token_count main "ye to des hai tera"]
+## puts "3) zzz $a zzz"
+# set b "3) zzz $a zzz"
+# puts $b
 puts " "
+
 
 if {[string bytelength $es_remote_command] < 20} {
     puts "setting remote command string locally,"
     set es_remote_command "ip=`ifconfig | grep inet | grep 10.174 | grep Bcast | awk '{ print \$2 }' | cut -d\":\" -f2`; hn=`hostname`; echo \"\$ip   \$hn\""
+}
+
+
+set_expect_out_pattern_matches 
+# Hmm, those variables in the tcl procedure only held their non-null values in that scope - TMH
+
+if { 1 } {
+    set expect_out(1,string) 101
+    set expect_out(2,string) 102
+    set expect_out(3,string) 103
+    set expect_out(4,string) 104
+    set expect_out(5,string) 105
+
+    set expect_out(6,string) 106
+    set expect_out(7,string) 107
+    set expect_out(8,string) 108
+    set expect_out(9,string) 109
 }
 
 
@@ -72,18 +130,72 @@ expect "*ssword: " { send "$es_passphrase\r" }
 
     expect $prompt
 
-    send $es_remote_command\r
+    send "$es_remote_command\r"
     expect "\r"
+
 
     puts "-- OPENING LOG FILE -- "
     log_file $es_logfile
-#    expect {[0-9]+\.[0-9]+\ \ \ ([a-zA-Z0-9\-])+}
-    expect "\r"
+
+#    expect "\r"
+    expect "_1"
+
     log_file
     puts "-- CLOSING LOG FILE -- "
 
-    puts "-- NOTE -- expecting prompt . . ."
+
+
+    puts "-- NOTE 1 -- expecting prompt . . ."
     expect $prompt
+
+
+if { 1 } {
+
+    puts "this expect script going on with further tests,"
+
+    puts "- NOTE 2 - to remote host $es_host sending 'ls' with carriage return . . ."
+    send "ls -f -d \[abcDM\]*\r"
+    expect {
+#        -re "(.*)xml" { puts [show_diag_tcl main "expecting \".*xml\", got:" 0]; puts [show_diag_tcl main $expect_out(buffer) 0] }
+#        -re "(.*)xml(.*)xml(.*)xml(.*)(.*)(.*)" { puts "- NOTE 2 - characters stored in expect's variable expect_out(1,string) are:"; puts $expect_out(1,string) }
+        "\r" { puts "matched a new-line after sending `ls` command,"; exp_continue }
+        -re "(.*)bin(.*)Desktop(.*)" { puts "matched pattern to break up remote `ls` results:"; puts $expect_out(1,string); puts $expect_out(2,string); exp_continue }
+        -re "(.*)Music\r" { puts "matched pattern ending line with string '(.*)Music'," }
+    }
+
+    send $ls_command
+    expect -re "(.*)bin(.*)Desktop(.*)"
+    puts "- NOTE 3 - splitting ls results on 'bin' and 'Desktop', \$expect_out(buffer) holds:"
+    puts "zzz out(buffer)"
+    puts $expect_out(buffer)
+    puts "zzz out(1,string)"
+    puts $expect_out(1,string)
+    puts "zzz out(2,string)"
+    puts $expect_out(2,string)
+    puts "zzz out(3,string)"
+    puts $expect_out(3,string)
+
+    send $ls_command
+    expect -re "(.*)Music\r"
+    puts "- NOTE 4 - looking at expect_out(n,string) for n in 1..3:"
+    puts "zzz out(buffer)"
+    puts $expect_out(buffer)
+    puts "zzz out(1,string)"
+    puts $expect_out(1,string)
+    puts "zzz out(2,string)"
+    puts $expect_out(2,string)
+    puts "zzz out(3,string)"
+    puts $expect_out(3,string)
+
+    send "\r"
+    expect $prompt
+
+
+    puts "done with extra tests,"
+} else {
+    puts "expect 'capture result' script skipping extra tests,"
+}
+
 
 
 puts "expect script done."
---
+++ test.tcl
@@ -0,0 +1,5 @@
+#!/usr/bin/tclsh
+
+## https://www.tutorialspoint.com/tcl-tk/tcl_basic_syntax.htm
+
+puts "Hello, World!"
